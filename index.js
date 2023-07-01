1919b02dadf47da28f0736a5f8e1082e7ac68398
elviraff <elvvvirk@mail.ru>
2023-07-01 21:27:51 +0300

    ATM

--- index.js
+++ index.js
@@ -155,3 +155,40 @@
     .map((item, index,arr) => (item%2 != 0) && (arr[index+1] % 2 !=0)&&(index!=arr.length-1) ? `${item}-` : item)
     .join('')
     }
+
+
+
+//task ATM cash
+    //const banknotes = [10, 50, 100, 200, 500, 1000, 2000, 5000]
+
+    // function atm(cash) {
+    //     8700/5000 = 1
+    //       3700/2000 = 1
+    //        1700/1000 = 1
+    
+    //   }
+  
+  // console.log(atm(12700));
+  // {
+  //   5000: 1,
+  //   2000: 1,
+  //   1000: 1,
+  //   100: 1,
+  //    50: 1,
+  //   10: 2
+  // }
+  function atm(cash) {
+        let res = {}
+        banknotes.sort((a,b)=> b-a).forEach(item => {
+            let count = Math.floor(cash/item)
+            if (count > 0) {
+                res[item] = count
+                cash = cash-item*count
+            } else {
+                res[item] = 0
+            }
+        })
+        return res
+    }
+
+  
